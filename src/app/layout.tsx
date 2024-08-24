28cd8cf4b44424952249998008963c8148992f67
Felix Darko Owusu <felix@feenix.technology>
2024-08-24 00:44:09 +0000

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import Header from '@/components/Header/Header'; // imported the header file so it can be accessed here for use
+import "./globals.css";
+
+const poppins = Poppins({ subsets: ["latin"],  // changed font family from inter to Poppins and added weight, style and variable
+  weight: ['400', '500', '700', '900'], 
+  style: ['italic', 'normal'], 
+  variable: '--font-poppins' 
+});
+
+export const metadata: Metadata = {
+  title: "Hotel Management App",
+  description: "Discover the best hotel rooms like never before",
+};
+
+/*
+  called the Poppins class to be used by the pages.
+  added the header and footer sections
+*/
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}> 
+        <main className="font-normal">
+          <Header />
+          {children}
+          {/* ?Footer */}
+        </main>
+      </body>
+    </html>
+  );
+}
