2f60cb6be25567bee79d6ed615874ee8f19b96ff
Felix Darko Owusu <felixowusu479@gmail.com>
2024-09-02 18:38:24 +0000

    gallery section

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,15 +1,18 @@
+import FeaturedRoom from "@/components/FeaturedRoom/FeaturedRoom";
+import Gallery from "@/components/Gallery/Gallery";
 import HeroSection from "@/components/HeroSection/HeroSection";
+import NewsLetter from "@/components/NewsLetter/NewsLetter";
 import PageSearch from "@/components/PageSearch/PageSearch";
 
 const Home = () => {
   return ( 
-  <>
-    <HeroSection />
-    <PageSearch />
-    {/* Featured Room */}
-    {/* Gallery */}
-    {/* Mews Letter */}
-  </>
+    <>
+      <HeroSection />
+      <PageSearch />
+      <FeaturedRoom />
+      <Gallery />
+      <NewsLetter />
+    </>
   );
 };
 
---
+++ src/components/Gallery/Gallery.tsx
@@ -0,0 +1,71 @@
+import Image from "next/image";
+
+const Gallery = () => {
+  return (
+    <div className='mx-auto container py-14 h-full'>
+      <div className='flex flex-wrap md:-m-2'>
+        <div className='flex w-1/2 flex-wrap'>
+          <div className='w-1/2 p-1 md:p-2 h-48'>
+            <Image
+              alt='gallery'
+              className='img'
+              src='/images/gallery-1.jpg'
+              width={200}
+              height={200}
+            />
+          </div>
+          <div className='w-1/2 p-1 md:p-2 h-48'>
+            <Image
+              alt='gallery'
+              className='img'
+              src='/images/gallery-2.jpg'
+              width={200}
+              height={200}
+            />
+          </div>
+          <div className='w-full p-1 md:p-2 h-48'>
+            <Image
+              alt='gallery'
+              className='img'
+              src='/images/hero-2.jpeg'
+              width={200}
+              height={200}
+              priority
+            />
+          </div>
+        </div>
+        <div className='flex w-1/2 flex-wrap'>
+          <div className='w-full p-1 md:p-2 h-48'>
+            <Image
+              alt='gallery'
+              className='img'
+              src='/images/gallery-5.jpg'
+              width={200}
+              height={200}
+            />
+          </div>
+          <div className='w-1/2 p-1 md:p-2 h-48'>
+            <Image
+              alt='gallery'
+              className='img'
+              src='/images/gallery-6.jpg'
+              width={200}
+              height={200}
+            />
+          </div>
+          <div className='w-1/2 p-1 md:p-2 h-48'>
+            <Image
+              alt='gallery'
+              className='img'
+              src='/images/gallery-3.jpg'
+              width={200}
+              height={200}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Gallery
