e4b186af8f91a3bd352002fd4b2b3e221193abd3
Felix Darko Owusu <felixowusu479@gmail.com>
2024-09-10 03:03:59 +0000

    data fetching

--- src/app/(web)/rooms/page.tsx
+++ src/app/(web)/rooms/page.tsx
@@ -5,6 +5,8 @@ import { useEffect, useState } from "react";
 import useSWR from "swr";
 
 import { getRooms } from "@/libs/apis";
+import { Room } from "@/models/room";
+import Search from "@/components/Search/Search";
 
 const Rooms = () => {
     const [roomTypeFIlter, setRoomTypeFilter] = useState("");
@@ -25,11 +27,53 @@ const Rooms = () => {
 
     const {data, error, isLoading} = useSWR("get/hotelRooms", fetchData);
 
-    if (error)
-    
-  return (
-    <div>Rooms</div>
-  )
-}
+    if (error) throw new Error('Cannot fetch data');
+    if (typeof data === undefined && !isLoading)
+      throw new Error('Cannot fetch data');
+
+    const filterRooms = (rooms: Room[]) => {
+      return rooms.filter(room => {
+        // Apply room type filter
+
+        if (
+          roomTypeFIlter &&
+          roomTypeFIlter.toLowerCase() !== 'all' &&
+          room.type.toLowerCase() !== roomTypeFIlter.toLowerCase()
+        ) {
+          return false;
+        }
+
+        // Apply search query filter
+
+        if (
+          searchQuery &&
+          !room.name.toLowerCase().includes(searchQuery.toLowerCase())
+        ) {
+          return false;
+        }
+
+        return true;
+      });
+    };
+
+    const filteredRooms = filterRooms(data || []);
+
+    return (
+      <div className="container mx-auto pt-10">
+        <Search 
+          roomTypeFilter={roomTypeFIlter}
+          searchQuery={searchQuery}
+          setRoomTypeFilter={setRoomTypeFilter}
+          setSearchQuery={setSearchQuery}
+        />
+
+        <div className="flex mt-20 justify-between flex-wrap">
+              {filteredRooms.map(room => (
+                <></>
+              ))}
+        </div>
+      </div>
+    );
+};
 
 export default Rooms
