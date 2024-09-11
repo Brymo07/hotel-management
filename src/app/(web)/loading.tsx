c0a7405c83f5791bc93fd4be0194debe5167932d
Felix Darko Owusu <felixowusu479@gmail.com>
2024-09-11 23:22:58 +0000

    data fetching

--- src/app/(web)/loading.tsx
+++ src/app/(web)/loading.tsx
@@ -1,5 +1,6 @@
-const loadingSpinner = () => <div className="flex items-center justify-center h-screen">
-    <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-gray-900" />
-</div>
-
-export default loadingSpinner;
+const LoadingSpinner = () => (
+    <div className='flex items-center justify-center h-screen'>
+      <div className='animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-gray-900' />
+    </div>
+  );
+  export default LoadingSpinner;
