729200b54db4b7f0164cb93516bd0ab9ee4e13cc
Felix Darko Owusu <felixowusu479@gmail.com>
2024-09-17 23:46:50 +0000

    book room cta

--- src/app/(web)/rooms/[slug]/page.tsx
+++ src/app/(web)/rooms/[slug]/page.tsx
@@ -1,45 +1,77 @@
-"use client";
+'use client';
 
-import { useState } from "react";
-import { getRoom } from "@/libs/apis";
+import useSWR from 'swr';
+import { MdOutlineCleaningServices } from 'react-icons/md';
+import { LiaFireExtinguisherSolid } from 'react-icons/lia';
+import { AiOutlineMedicineBox } from 'react-icons/ai';
+import { GiSmokeBomb } from 'react-icons/gi';
+import { useState } from 'react';
 
-import useSWR from "swr";
-import LoadingSpinner from "../../loading";
-import HotelPhotoGallery from "@/components/HotelPhotoGallery/HotelPhotoGallery";
-import { MdOutlineCleaningServices } from "react-icons/md";
-import { LiaFireExtinguisherSolid } from "react-icons/lia";
-import { AiOutlineMedicineBox } from "react-icons/ai";
-import { GiSmokeBomb } from "react-icons/gi";
-import BookRoomCta from "@/components/BookRoomCta/BookRoomCta";
+import { getRoom } from '@/libs/apis';
+import LoadingSpinner from '../../loading';
+import HotelPhotoGallery from '@/components/HotelPhotoGallery/HotelPhotoGallery';
+import BookRoomCta from '@/components/BookRoomCta/BookRoomCta';
+import toast from 'react-hot-toast';
 
 const RoomDetails = (props: { params: { slug: string } }) => {
   const {
     params: { slug },
   } = props;
 
-  const [checkinDate, setCheckinDate] = useState<Date | null>(null)
-  const [checkoutDate, setCheckoutDate] = useState<Date | null>(null)
+  const [checkinDate, setCheckinDate] = useState<Date | null>(null);
+  const [checkoutDate, setCheckoutDate] = useState<Date | null>(null);
+  const [adults, setAdults] = useState(1);
+  const [noOfChildren, setNoOfChildren] = useState(0);
 
   const fetchRoom = async () => getRoom(slug);
 
   const { data: room, error, isLoading } = useSWR('/api/room', fetchRoom);
 
   if (error) throw new Error('Cannot fetch data');
-  if (typeof room === undefined && !isLoading)
+  if (typeof room === 'undefined' && !isLoading)
     throw new Error('Cannot fetch data');
 
   if (!room) return <LoadingSpinner />;
-  
+
+  const calcMinCheckoutDate = () => {
+    if (checkinDate) {
+      const nextDay = new Date(checkinDate);
+      nextDay.setDate(nextDay.getDate() + 1);
+      return nextDay;
+    }
+    return null;
+  };
+
+  const handleBookNowClick = async () => {
+    if (!checkinDate || !checkoutDate)
+      return toast.error('Please provide checkin / checkout date');
+
+    if (checkinDate > checkoutDate)
+      return toast.error('Please choose a valid checkin period');
+
+    const numberOfDays = calcNumDays();
+
+    const hotelRoomSlug = room.slug.current;
+
+    
+  };
+
+  const calcNumDays = () => {
+    if (!checkinDate || !checkoutDate) return;
+    const timeDiff = checkoutDate.getTime() - checkinDate.getTime();
+    const noOfDays = Math.ceil(timeDiff / (24 * 60 * 60 * 1000));
+    return noOfDays;
+  };
+
   return (
     <div>
       <HotelPhotoGallery photos={room.images} />
 
-      <div className="container mx-auto mt-20">
-        <div className="md:grid md:grid-cols-12 gap-10 px-3">
-          <div className="md:col-span-8 md:w-full">
-            {/* Hotel Information */}
+      <div className='container mx-auto mt-20'>
+        <div className='md:grid md:grid-cols-12 gap-10 px-3'>
+          <div className='md:col-span-8 md:w-full'>
             <div>
-            <h2 className='font-bold text-left text-lg md:text-2xl'>
+              <h2 className='font-bold text-left text-lg md:text-2xl'>
                 {room.name} ({room.dimension})
               </h2>
               <div className='flex my-11'>
@@ -55,23 +87,23 @@ const RoomDetails = (props: { params: { slug: string } }) => {
                   </div>
                 ))}
               </div>
-              <div className="mb-11">
-                <h2 className="font-bold text-3xl mb-2">Description</h2>
+              <div className='mb-11'>
+                <h2 className='font-bold text-3xl mb-2'>Description</h2>
                 <p>{room.description}</p>
               </div>
-              <div className="mb-11">
-                <h2 className="font-bold text-3xl mb-2">Offered Amenities</h2>
-                <div className="grid grid-cols-2">
+              <div className='mb-11'>
+                <h2 className='font-bold text-3xl mb-2'>Offered Amenities</h2>
+                <div className='grid grid-cols-2'>
                   {room.offeredAmenities.map(amenity => (
                     <div
-                     key={amenity._key} 
-                     className="flex items-center md:my-0 my-1"
+                      key={amenity._key}
+                      className='flex items-center md:my-0 my-1'
                     >
                       <i className={`fa-solid ${amenity.icon}`}></i>
                       <p className='text-xs md:text-base ml-2'>
                         {amenity.amenity}
                       </p>
-                  </div>
+                    </div>
                   ))}
                 </div>
               </div>
@@ -101,7 +133,6 @@ const RoomDetails = (props: { params: { slug: string } }) => {
                 </div>
               </div>
 
-              {/* Reviews Section */}
               <div className='shadow dark:shadow-white rounded-lg p-6'>
                 <div className='items-center mb-4'>
                   <p className='md:text-lg font-semibold'>Customer Reviews</p>
@@ -113,13 +144,22 @@ const RoomDetails = (props: { params: { slug: string } }) => {
             </div>
           </div>
 
-          <div className="md:col-span-4 rounded-xl shadow-lg dark:shadow-white sticky top-10 h-fit overflow-auto">
+          <div className='md:col-span-4 rounded-xl shadow-lg dark:shadow dark:shadow-white sticky top-10 h-fit overflow-auto'>
             <BookRoomCta
-              discount={room.discount} 
-              price={room.price} 
+              discount={room.discount}
+              price={room.price}
               specialNote={room.specialNote}
               checkinDate={checkinDate}
               setCheckinDate={setCheckinDate}
+              checkoutDate={checkoutDate}
+              setCheckoutDate={setCheckoutDate}
+              calcMinCheckoutDate={calcMinCheckoutDate}
+              adults={adults}
+              noOfChildren={noOfChildren}
+              setAdults={setAdults}
+              setNoOfChildren={setNoOfChildren}
+              isBooked={room.isBooked}
+              handleBookNowClick={handleBookNowClick}
             />
           </div>
         </div>
@@ -128,4 +168,4 @@ const RoomDetails = (props: { params: { slug: string } }) => {
   );
 };
 
-export default RoomDetails
+export default RoomDetails;
