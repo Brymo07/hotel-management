4817613aed745fb5977bb34050a1e33e98e0a25e
Felix Darko Owusu <felixowusu479@gmail.com>
2024-10-08 23:24:35 +0000

    booking rooms

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,53 +1,80 @@
-import { createBooking } from "@/libs/apis";
-import { NextResponse } from "next/server";
-import Stripe from "stripe";
+import { NextResponse } from 'next/server';
+import Stripe from 'stripe';
 
-const checkout_session_completed = "checkout.session.completed";
+import { createBooking, updateHotelRoom } from '@/libs/apis';
+
+const checkout_session_completed = 'checkout.session.completed';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-    apiVersion: '2024-06-20',
+  apiVersion: '2024-06-20'
 });
 
 export async function POST(req: Request, res: Response) {
-    const reqBody = await req.text()
-    const sig = req.headers.get("stripe-signature")
-    const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+  const reqBody = await req.text();
+  const sig = req.headers.get('stripe-signature');
+  const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
 
-    let event: Stripe.Event;
+  let event: Stripe.Event;
 
-    try {
-        if (!sig || !webhookSecret) return;
-        event = stripe.webhooks.constructEvent(reqBody, sig, webhookSecret);
-    } catch (error: any) {
-        return new NextResponse(`Webhook Error: ${error.message}`, { status: 500 });
-    }
+  try {
+    if (!sig || !webhookSecret) return;
+    event = stripe.webhooks.constructEvent(reqBody, sig, webhookSecret);
+  } catch (error: any) {
+    return new NextResponse(`Webhook Error: ${error.message}`, { status: 500 });
+  }
 
-    // load the event
-    switch (event.type) {
-        case checkout_session_completed:
-            const session = event.data.object;
-            
-            const {
-                // @ts-ignore
-                metadata: {
-                    
-                },
-            } = session;
+  // Handle event
+  switch (event.type) {
+    case checkout_session_completed:
+      const session = event.data.object as Stripe.Checkout.Session;
 
-            // create a booking
-            // await createBooking({adults, checkinDate})
+      // Safely accessing the metadata
+      const metadata = session.metadata;
 
-            return NextResponse.json('Booking successful', {
-                status: 200,
-                statusText: 'Booking successful',
-            })
+      if (!metadata) {
+        console.error('Metadata is missing in the session');
+        return new NextResponse('Metadata is missing', { status: 400 });
+      }
 
-        default:
-            console.log(`Unhanled event type ${event.type}`)
-    }
+      const {
+        adults,
+        checkinDate,
+        checkoutDate,
+        children,
+        hotelRoom,
+        numberOfDays,
+        user,
+        discount,
+        totalPrice,
+      } = metadata;
 
-    return NextResponse.json('Event Received', {
+      // create a booking
+      await createBooking({
+        adults: Number(adults),
+        checkinDate,
+        checkoutDate,
+        children: Number(children),
+        hotelRoom,
+        numberOfDays: Number(numberOfDays),
+        discount: Number(discount),
+        totalPrice: Number(totalPrice),
+        user,
+      });
+
+      // Update hotel Room
+      await updateHotelRoom(hotelRoom);
+
+      return NextResponse.json('Booking successful', {
         status: 200,
-        statusText: 'Event Received',
-    })
+        statusText: 'Booking Successful',
+      });
+
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  return NextResponse.json('Event Received', {
+    status: 200,
+    statusText: 'Event Received',
+  });
 }
--- src/libs/apis.ts
+++ src/libs/apis.ts
@@ -2,6 +2,7 @@ import { CreateBookingDto, Room } from "@/models/room";
 import sanityClient from "./sanity";
 import * as queries from "./sanityQueries";
 import axios from "axios";
+import { patch } from "sanity/migrate";
 
 export async function getFeaturedRoom() {
     const result = await sanityClient.fetch<Room>(
@@ -61,6 +62,29 @@ export const createBooking = async ({
       },
     ],
   };
+
+  const { data } = await axios.post(
+    `https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2021-10-21/data/mutate/${process.env.NEXT_PUBLIC_SANITY_DATASET}`,
+    mutation,
+    { headers: { Authorization: `Bearer ${process.env.SANITY_STUDIO_TOKEN}` } }
+  );
+
+  return data;
+};
+
+export const updateHotelRoom = async (hotelRoomId: string) => {
+  const mutation = {
+    mutations: [
+      {
+        patch: {
+          id: hotelRoomId,
+          set: {
+            IsBooked: true
+          }
+        }
+      }
+    ]
+  }
 
   const { data } = await axios.post(
     `https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2021-10-21/data/mutate/${process.env.NEXT_PUBLIC_SANITY_DATASET}`,
