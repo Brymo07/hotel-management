f21e8ce1cb215f3d7e957649ab8b991b292521e3
Felix Darko Owusu <felixowusu479@gmail.com>
2024-10-18 23:32:13 +0000

    reviews api

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,4 +1,4 @@
-import { getUserData } from "@/libs/apis";
+import { checkReviewExists, getUserData } from "@/libs/apis";
 import { authOptions } from "@/libs/auth";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
@@ -18,4 +18,31 @@ export async function GET(req:Request, res: Response) {
     } catch (error) {
         return new NextResponse("Unable to fetch", {status: 500})
     }
+};
+
+export async function POST(req: Request, res: Response) {
+    const session = await getServerSession(authOptions);
+    
+    if (!session) {
+        return new NextResponse('Authentication Required', { status: 500 });
+    }
+
+    const {roomId, reviewText, ratingValue} = await req.json();
+
+    if (!roomId || !reviewText || !ratingValue) {
+        return new NextResponse('All fields are required', { status: 400 });
+    }
+
+    const userId = session.user.id;
+    try {
+        // Check if review already exists
+        const alreadyExists = await checkReviewExists(userId, roomId)
+
+        console.log(alreadyExists);
+    } catch (error: any) {
+        console.log('Error Updating', error);
+        return new NextResponse('Unable to create review', { status: 400 });
+    }
+
+    
 }
